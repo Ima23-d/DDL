176271e0eab3ab16b8cfb0ea3caac1c013fd0d2e
Arthur Franco <153031237+Ima23-d@users.noreply.github.com>
2025-09-03 16:08:31 -0300

    Add files via upload

---
+++ DDL/DDL.sql
@@ -0,0 +1,48 @@
+CREATE TABLE Alunos (
+    ID_Aluno INT PRIMARY KEY,
+    Nome_Aluno VARCHAR(100),
+    CPF VARCHAR(14) UNIQUE,
+    Data_Nascimento DATE,
+    Idade INT,
+    Peso DECIMAL(5,2),
+    Gordura_Corporal DECIMAL(5,2),
+    Nivel VARCHAR(20),
+    Deficiencia VARCHAR(100),
+    Email VARCHAR(100)
+);
+
+CREATE TABLE Instrutores (
+    ID_Instrutor INT PRIMARY KEY,
+    Nome VARCHAR(100),
+    CREF VARCHAR(20) UNIQUE
+);
+
+CREATE TABLE Planos (
+    ID_Planos INT PRIMARY KEY,
+    Nome_Plano VARCHAR(100),
+    Descricao TEXT,
+    Valor DECIMAL(10,2)
+);
+
+CREATE TABLE Treinos (
+    ID_Treinos INT PRIMARY KEY,
+    Especificacoes TEXT,
+    ID_Instrutor INT,
+    FOREIGN KEY (ID_Instrutor) REFERENCES Instrutores(ID_Instrutor)
+);
+
+CREATE TABLE Realiza (
+    ID_Aluno INT,
+    ID_Treinos INT,
+    PRIMARY KEY (ID_Aluno, ID_Treinos),
+    FOREIGN KEY (ID_Aluno) REFERENCES Alunos(ID_Aluno),
+    FOREIGN KEY (ID_Treinos) REFERENCES Treinos(ID_Treinos)
+);
+
+CREATE TABLE Escolhe (
+    ID_Aluno INT,
+    ID_Planos INT,
+    PRIMARY KEY (ID_Aluno, ID_Planos),
+    FOREIGN KEY (ID_Aluno) REFERENCES Alunos(ID_Aluno),
+    FOREIGN KEY (ID_Planos) REFERENCES Planos(ID_Planos)
+);
